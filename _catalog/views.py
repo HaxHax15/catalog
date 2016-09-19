717816ab48f5f89b606e9ff670ce825284c68688
vitaliy <hax.hax@mail.ru>
2016-09-19 14:12:55 +0300

    123

--- _catalog/views.py
+++ _catalog/views.py
@@ -5,7 +5,7 @@ from .models import Country
 def countries(request):
     countries_list = Country.objects.all()
     context = {'countries': countries_list, }
-    return render(request, 'catalog/index.html', context)
+    return render(request, 'catalog/countries.html', context)
 
 
 def index(request):
