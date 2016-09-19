8537e4c6fe72a0df046a6ed14531f5e8c0ce44a4
vitaliy <hax.hax@mail.ru>
2016-09-19 14:05:29 +0300

    123

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='catalog',
+    version='',
+    packages=['catalog', '_catalog', '_catalog.migrations'],
+    url='',
+    license='',
+    author='vitaliy',
+    author_email='',
+    description=''
+)
